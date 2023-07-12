d87ecdfaa467db0177aae6262f68cc7ba781b534
Rodrigo Salto <97195207+saltorodrigo@users.noreply.github.com>
2023-07-11 22:56:58 -0300

    Update productos 1

--- js/productos.js
+++ js/productos.js
@@ -1,74 +1,67 @@
-<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
-<script>
-  const { createApp } = Vue;
-
-  const app = createApp({
+const { createApp } = Vue
+createApp({
     data() {
-      return {
-        productos: [],
-        url: 'http://rodrigosalto.pythonanywhere.com/productos',
-        error: false,
-        cargando: true,
-        id: 0,
-        nombre: "",
-        imagen: "",
-        stock: 0,
-        precio: 0,
-      }
+        return {
+            productos: [],
+            url:'http://rodrigosalto.pythonanywhere.com/productos',
+            error: false,
+            cargando: true,
+            id: 0,
+            nombre: "",
+            imagen: "",
+            stock: 0,
+            precio: 0,
+        }
     },
     methods: {
-      fetchData(url) {
-        fetch(url)
-          .then(response => response.json())
-          .then(data => {
-            this.productos = data;
-            this.cargando = false;
-          })
-          .catch(err => {
-            console.error(err);
-            this.error = true;
-          })
-      },
-      eliminar(producto) {
-        const url = this.url + '/' + producto;
-        var options = {
-          method: 'DELETE',
-        };
-        fetch(url, options)
-          .then(res => res.text()) // or res.json()
-          .then(res => {
-            location.reload();
-          })
-      },
-      grabar() {
-        let producto = {
-          nombre: this.nombre,
-          precio: this.precio,
-          stock: this.stock,
-          imagen: this.imagen
-        };
-        var options = {
-          body: JSON.stringify(producto),
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          redirect: 'follow'
-        };
-        fetch(this.url, options)
-          .then(() => {
-            alert("Registro grabado");
-            window.location.href = "index.html";
-          })
-          .catch(err => {
-            console.error(err);
-            alert("Error al Grabar");
-          })
-      }
+        fetchData(url) {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    this.productos = data;
+                    this.cargando = false
+                })
+                .catch(err => {
+                    console.error(err);
+                    this.error = true
+                })
+        },
+        eliminar(producto) {
+            const url = this.url + '/' + producto;
+            var options = {
+                method: 'DELETE',
+            }
+            fetch(url, options)
+                .then(res => res.text()) // or res.json()
+                .then(res => {
+                    location.reload();
+                })
+        },
+        grabar() {
+            let producto = {
+                nombre: this.nombre,
+                precio: this.precio,
+                stock: this.stock,
+                imagen: this.imagen
+            }
+            var options = {
+                body: JSON.stringify(producto),
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                redirect: 'follow'
+            }
+            fetch(this.url, options)
+                .then(function () {
+                    alert("Registro grabado")
+                    window.location.href = "/index.html";
+                })
+                .catch(err => {
+                    console.error(err);
+                    alert("Error al Grabarr")
+                })
+        }
     },
     created() {
-      this.fetchData(this.url);
+        this.fetchData(this.url)
     },
-  });
-
-  app.mount('#app');
-</script>
-
+}).mount('#app')
