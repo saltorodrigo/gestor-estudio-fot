ff371c2ea1ab07c1be55ea868e337a11c54ef830
Rodrigo Salto <97195207+saltorodrigo@users.noreply.github.com>
2023-07-11 22:09:00 -0300

    Update productos.js

--- js/productos.js
+++ js/productos.js
@@ -3,12 +3,9 @@ createApp({
     data() {
         return {
             productos: [],
-            url:'http://localhost:5000/productos',
-            // si el backend esta corriendo local usar localhost 5000(si no lo subieron a pythonanywhere)
-            // url: 'http://promero.pythonanywhere.com/productos/', // si ya lo subieron a pythonanywhere
+            url:'http://rodrigosalto.pythonanywhere.com',
             error: false,
             cargando: true,
-            /*atributos para el guardar los valores del formulario */
             id: 0,
             nombre: "",
             imagen: "",
